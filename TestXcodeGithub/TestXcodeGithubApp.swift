bf674ece5f184842307ab554fd6d53a1c2240231
janelleenqi <110933149+janelleenqi@users.noreply.github.com>
2024-03-17 10:31:53 +0800

    1st commit

---
+++ TestXcodeGithub/TestXcodeGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestXcodeGithubApp.swift
+//  TestXcodeGithub
+//
+//  Created by Janelle Liow on 17/3/24.
+//
+
+import SwiftUI
+
+@main
+struct TestXcodeGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
